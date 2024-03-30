0b1728554f247a451cc97ad95af0d166b6899d62
mark <abdulrasikm0@gmail.com>
2024-03-30 22:07:36 +0530

    Slight changes

--- Bot/slash_commands.py
+++ Bot/slash_commands.py
@@ -1,41 +1,45 @@
 import aiohttp
 import discord
 
-
-async def hello(interaction: discord.Integration):
-    await interaction.response.send_message(f"Hey yo,{interaction.user.mention}")
-
-
-async def neko(interaction: discord.Integration):
-    url = 'https://api.waifu.pics/sfw/neko'
-    data = await fetch_json(url)
-    url = data['url']
-    await interaction.response.send_message(url)
+try:
+    async def hello(interaction: discord.Integration):
+        await interaction.response.send_message(f"Hey yo,{interaction.user.mention}")
 
 
-async def waifu(interaction: discord.Integration):
-    url = 'https://api.waifu.pics/sfw/waifu'
-    data = await fetch_json(url)
-    url = data['url']
-    await interaction.response.send_message(url)
+    async def neko(interaction: discord.Integration):
+        url = 'https://api.waifu.pics/sfw/neko'
+        data = await fetch_json(url)
+        url = data['url']
+        await interaction.response.send_message(url)
 
 
-async def shinobu(interaction: discord.Integration):
-    url = 'https://api.waifu.pics/sfw/shinobu'
-    data = await fetch_json(url)
-    url = data['url']
-    await interaction.response.send_message(url)
+    async def waifu(interaction: discord.Integration):
+        url = 'https://api.waifu.pics/sfw/waifu'
+        data = await fetch_json(url)
+        url = data['url']
+        await interaction.response.send_message(url)
 
 
-async def megumin(interaction: discord.Integration):
-    url = 'https://api.waifu.pics/sfw/megumin'
-    data = await fetch_json(url)
-    url = data['url']
-    await interaction.response.send_message(url)
+    async def shinobu(interaction: discord.Integration):
+        url = 'https://api.waifu.pics/sfw/shinobu'
+        data = await fetch_json(url)
+        url = data['url']
+        await interaction.response.send_message(url)
 
 
-async def say(interaction: discord.Integration, describe: str):
-    await interaction.response.send_message(f"{interaction.user.mention} said: `{describe}`")
+    async def megumin(interaction: discord.Integration):
+        url = 'https://api.waifu.pics/sfw/megumin'
+        data = await fetch_json(url)
+        url = data['url']
+        await interaction.response.send_message(url)
+
+
+    async def say(interaction: discord.Integration, describe: str):
+        await interaction.response.send_message(f"{interaction.user.mention} said: `{describe}`")
+
+except discord.errors.NotFound:
+    print('Try again!')
+
 
 
 async def fetch_json(url):
