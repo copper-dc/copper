5b884ced03a75345f2a1a9ac93e59a2d1fde9600
mark <abdulrasikm0@gmail.com>
2024-04-08 23:24:58 +0530

    (Experimental) Some lines of code are to be removed

--- Bot/slash_commands.py
+++ Bot/slash_commands.py
@@ -1,54 +1,54 @@
 import aiohttp
 import discord
 import random
-from Rewards import award_points
 
-waifuBaseURL = "https://api.waifu.pics/"
-RPS = ['rock','paper','scissors']
 
-embedMessage = discord.Embed(colour=discord.Colour.random())
+# waifuBaseURL = "https://api.waifu.pics/"
+# RPS = ['rock','paper','scissors']
 
-try:
-    async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message(f"Hey yo,{interaction.user.mention}")
+# embedMessage = discord.Embed(colour=discord.Colour.random())
 
-    async def slashgirls(interaction:discord.Integration, imgCategory: str, girltype: str):
-        slashgirlsEmbeds = discord.Embed(title="Enjoy your slash girl 🤤",colour=discord.Colour.random())
+# try:
+#     async def hello(interaction: discord.Interaction):
+#         await interaction.response.send_message(f"Hey yo,{interaction.user.mention}")
+
+#     async def slashgirls(interaction:discord.Integration, imgCategory: str, girltype: str):
+#         slashgirlsEmbeds = discord.Embed(title="Enjoy your slash girl 🤤",colour=discord.Colour.random())
         
-        if (girltype== 'neko'):
-            imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
-            updatedURL = waifuBaseURL+imageCategory+"/neko"
-            data = await fetch_json(updatedURL)
-            imgurl = data['url']
-            slashgirlsEmbeds.set_image(url=imgurl)
-            await interaction.response.send_message(embed=slashgirlsEmbeds)
-        elif (girltype == 'waifu'):
-            imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
-            updatedURL = waifuBaseURL+imageCategory+"/waifu"
-            data = await fetch_json(updatedURL)
-            imgurl = data['url']
-            slashgirlsEmbeds.set_image(url=imgurl)
-            await interaction.response.send_message(embed=slashgirlsEmbeds)
-        elif (girltype=='megumin'):
-            try:
-                imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
-                updatedURL = waifuBaseURL+imageCategory+"/megumin"
-                data = await fetch_json(updatedURL)
-                imgurl = data['url']
-                slashgirlsEmbeds.set_image(url=imgurl)
-                await interaction.response.send_message(embed=slashgirlsEmbeds)
-            except Exception as e:
-                await interaction.response.send_message("There is no NSFW image for megumin")
-        elif (girltype == 'shinobu'):
-            try:
-                imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
-                updatedURL = waifuBaseURL+imageCategory+"/shinobu"
-                data = await fetch_json(updatedURL)
-                imgurl = data['url']
-                slashgirlsEmbeds.set_image(url=imgurl)
-                await interaction.response.send_message(embed=slashgirlsEmbeds)
-            except Exception as e:
-                await interaction.response.send_message("There is no NSFW image for shinobu")
+#         if (girltype== 'neko'):
+#             imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
+#             updatedURL = waifuBaseURL+imageCategory+"/neko"
+#             data = await fetch_json(updatedURL)
+#             imgurl = data['url']
+#             slashgirlsEmbeds.set_image(url=imgurl)
+#             await interaction.response.send_message(embed=slashgirlsEmbeds)
+#         elif (girltype == 'waifu'):
+#             imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
+#             updatedURL = waifuBaseURL+imageCategory+"/waifu"
+#             data = await fetch_json(updatedURL)
+#             imgurl = data['url']
+#             slashgirlsEmbeds.set_image(url=imgurl)
+#             await interaction.response.send_message(embed=slashgirlsEmbeds)
+#         elif (girltype=='megumin'):
+#             try:
+#                 imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
+#                 updatedURL = waifuBaseURL+imageCategory+"/megumin"
+#                 data = await fetch_json(updatedURL)
+#                 imgurl = data['url']
+#                 slashgirlsEmbeds.set_image(url=imgurl)
+#                 await interaction.response.send_message(embed=slashgirlsEmbeds)
+#             except Exception as e:
+#                 await interaction.response.send_message("There is no NSFW image for megumin")
+#         elif (girltype == 'shinobu'):
+#             try:
+#                 imageCategory = "sfw" if imgCategory == "sfw" else "nsfw"
+#                 updatedURL = waifuBaseURL+imageCategory+"/shinobu"
+#                 data = await fetch_json(updatedURL)
+#                 imgurl = data['url']
+#                 slashgirlsEmbeds.set_image(url=imgurl)
+#                 await interaction.response.send_message(embed=slashgirlsEmbeds)
+#             except Exception as e:
+#                 await interaction.response.send_message("There is no NSFW image for shinobu")
 
 
 # Games in Slash Commands
@@ -56,60 +56,60 @@ try:
 #  2. Gamble
 #  3. Slot Machine
             
-    async def rps(interaction:discord.Interaction, choices: str):
-        user_id = interaction.user.id
-        username = interaction.user.name
+    # async def rps(interaction:discord.Interaction, choices: str):
+    #     user_id = interaction.user.id
+    #     username = interaction.user.name
 
-        bot_choice = random.choice(RPS)
-        if (choices.value == bot_choice):
-            counter = 'That\'s a tie! Once more!!.'
-            await interaction.response.send_message(f"{bot_choice}. {counter}")
-        elif (choices.value == 'rock'):
-            if bot_choice == 'paper':
-                counter = 'Hehe, I win, Better luck next time~'
-                await interaction.response.send_message(f"{bot_choice}. {counter}")
-            else:
-                counter = 'Aw... , Lucky you!📈'
-                award_points(user_id,username,10)
-                await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #     bot_choice = random.choice(RPS)
+    #     if (choices.value == bot_choice):
+    #         counter = 'That\'s a tie! Once more!!.'
+    #         await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #     elif (choices.value == 'rock'):
+    #         if bot_choice == 'paper':
+    #             counter = 'Hehe, I win, Better luck next time~'
+    #             await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #         else:
+    #             counter = 'Aw... , Lucky you!📈'
+    #             award_points(user_id,username,10)
+    #             await interaction.response.send_message(f"{bot_choice}. {counter}")
 
-        elif (choices.value == 'paper'):
-            if bot_choice == 'scissors':
-                counter = 'Hehe, I win, Better luck next time~'
-                await interaction.response.send_message(f"{bot_choice}. {counter}")
-            else:
-                counter = 'Aw... , Lucky you!📈'
-                award_points(user_id,username,1)
-                await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #     elif (choices.value == 'paper'):
+    #         if bot_choice == 'scissors':
+    #             counter = 'Hehe, I win, Better luck next time~'
+    #             await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #         else:
+    #             counter = 'Aw... , Lucky you!📈'
+    #             award_points(user_id,username,1)
+    #             await interaction.response.send_message(f"{bot_choice}. {counter}")
 
-        elif (choices.value == 'scissors'):
-            if bot_choice == 'rock':
-                counter = 'Hehe, I win, Better luck next time~'
-                await interaction.response.send_message(f"{bot_choice}. {counter}")
-            else:
-                counter = 'Aw... , Lucky you!📈'
-                award_points(user_id,username,1)
-                await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #     elif (choices.value == 'scissors'):
+    #         if bot_choice == 'rock':
+    #             counter = 'Hehe, I win, Better luck next time~'
+    #             await interaction.response.send_message(f"{bot_choice}. {counter}")
+    #         else:
+    #             counter = 'Aw... , Lucky you!📈'
+    #             award_points(user_id,username,1)
+    #             await interaction.response.send_message(f"{bot_choice}. {counter}")
         
             
-        else:
-            counter = 'Enter Valid Input Bozo'
-            await interaction.response.send_message(counter)
+    #     else:
+    #         counter = 'Enter Valid Input Bozo'
+    #         await interaction.response.send_message(counter)
 
 
 
 
 
-    async def say(interaction: discord.Interaction, describe: str):
-        await interaction.response.send_message(f"{interaction.user.mention} said: `{describe}`")
+    # async def say(interaction: discord.Interaction, describe: str):
+    #     await interaction.response.send_message(f"{interaction.user.mention} said: `{describe}`")
 
-except discord.errors.NotFound:
-    print('Try again!')
+# except discord.errors.NotFound:
+#     print('Try again!')
 
 
 
-async def fetch_json(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            return data
+# async def fetch_json(url):
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url) as response:
+#             data = await response.json()
+#             return data
